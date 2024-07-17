d81a3688ea9e70abb93103c729f3a0e312d23874
Supriyo Pal <131426815+Supriyoiiest@users.noreply.github.com>
2024-07-17 14:29:16 +0530

    added ca model

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import matplotlib as plt
 from lib.preprocessing import CleanDataReturn
 from lib.ml_pred import evaluate_ch4
 import pandas as pd
+from lib.ca_use import usedCA
+import numpy as np
 
 def main():
     data = pd.read_csv('D:/merged_data_1.csv')
     data['Time'] = pd.to_datetime(data['Time'], format='%d-%m-%Y %H:%M')
     data_cleaned = CleanDataReturn(data)
-    data_cleaned['Predicted CH4 (ppm)_2'] = data_cleaned.apply(lambda row: evaluate_ch4(row['Coal'], row['RH (%)'], row['Temp (deg C)']), axis=1)
+    data_cleaned['Predicted CH4 (ppm)'] = data_cleaned.apply(lambda row: evaluate_ch4(row['Coal'], row['RH (%)'], row['Temp (deg C)']), axis=1)
+    predicted_ch4_array_alt = np.array(data_cleaned['Predicted CH4 (ppm)'])
+    for_value=usedCA(predicted_ch4_array_alt)
     plt.figure.Figure(figsize=(12, 6))
     data.drop(data.index[-1], inplace=True)
     plt.pyplot.plot(data['Time'], data_cleaned['CH4 (ppm)'], color='blue', label='Actual CH4 (ppm)')
-    plt.pyplot.plot(data['Time'], data_cleaned['Predicted CH4 (ppm)_2'], color='green', linestyle='dashed', label='Predicted CH4 (ppm) - Method 2')
+    plt.pyplot.plot(data['Time'], for_value , color='green', linestyle='dashed', label='Predicted CH4 (ppm)')
     plt.pyplot.xlabel('Time')
     plt.pyplot.ylabel('CH4 (ppm)')
     plt.pyplot.title('Actual vs Predicted CH4 (ppm) over Time')
